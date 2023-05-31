1a912b887599744433187a72d06803bc5c2298db
proxima424 <darpitrangari42@gmail.com>
2023-05-31 22:17:43 +0530

    wowww

---
+++ theme.config.js
@@ -0,0 +1,17 @@
+export default {
+    head : (
+        <head>
+        <meta property="og:title" content="UniswapV3 Core" />
+        <title>UniswapV3 Core</title>
+        </head>
+    ),
+    project : {
+        link : "https://github.com/Uniswap/v3-core",
+    },
+    title : "UniswapV3 Core",
+    titleSuffix: "- UniswapV3 Code",
+    darkMode: true,
+    search: true,
+    footer: <p>By @DefiDeepDive.</p>,
+    footerText: "Solady documentation",
+}
